76ad11efaf6e4bed7864f3ee1f904516f2439713
Christoffer Lantz <christoffer.lantz@androidbutiken.net>
2020-05-20 15:35:05 +0200

    Updated example so you can edit body as well

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,11 @@
  */
 
 // You can delete this file if you're not using it
+window.PHRASEAPP_ENABLED = true;
+window.PHRASEAPP_CONFIG = {
+  projectId: "",
+  prefix: "[[__",
+  suffix: "__]]",
+  fullReparse: true,
+  autoLowercase: false
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,20 @@
 import React from 'react';
 import { graphql } from 'gatsby';
-import { Translation } from 'react-i18next';
-import { Link, withI18next } from '@wapps/gatsby-plugin-i18next';
-
+import { Link } from '@wapps/gatsby-plugin-i18next';
+import { withNamespaces } from 'react-i18next-phraseapp';
 import Layout from '../components/layout';
+import withI18nextPhraseApp from "../i18n/with18next";
 
-const IndexPage = () => (
-  <Translation>
-    {t => (
-      <Layout>
-        <h1>{t('Hi people')}</h1>
-        <p>{t('Welcome to your new Gatsby site.')}</p>
-        <p>{t('Now go build something great.')}</p>
-        <Link to="/page-2/">{t('Go to page 2')}</Link>
-      </Layout>
-    )}
-  </Translation>
+const IndexPage = ({t}) => (
+  <Layout>
+    <h1>{t('Hi people')}</h1>
+    <p>{t('Welcome to your new Gatsby site.')}</p>
+    <p>{t('Now go build something great.')}</p>
+    <Link to="/page-2/">{t('Go to page 2')}</Link>
+  </Layout>
 );
 
-export default withI18next()(IndexPage);
+export default withI18nextPhraseApp()(withNamespaces()(IndexPage));
 
 export const query = graphql`
   query($lng: String!) {
